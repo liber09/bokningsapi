7ffdd2a68756110a5d4ac2e8b5fe041f217fbaa2
liber09 <linda.bergsangel@hotmail.com>
2023-11-16 19:14:41 +0100

    Almost there. NumberOfRooms is 0 when it shouldn't

--- functions/getBookings/index.js
+++ functions/getBookings/index.js
@@ -1,30 +1,48 @@
 const AWS = require('aws-sdk');
 const { sendResponse } = require('../../responses/index');
 const db = new AWS.DynamoDB.DocumentClient();
-
+const bookedRoomResponse = [];
 exports.handler = async (event, context) => {
-
     const params = {
         TableName: 'room-db',
-        };
+    };
+
+    const params2 = {
+      TableName: 'booking-db'
+    }
 
     try {
+        const rooms = await db.scan(params).promise();
+        const bookings = await db.scan(params2).promise();
 
-        // Get all items
-        const result = await db.scan(params).promise();
-
-        // Filter out all rooms that are contains booked
-        const bookedRooms = result.Items.filter(item => item.dates.length > 0)
-
+        
+        
+        bookings.Items.forEach(booking => {
+          const bookedRooms = rooms.Items
+            .filter(item => item.dates && item.dates.length > 0 && 
+              item.booked.bookingNumber === booking.bookingNumber);
+            let bookingInformation = {
+              "BookingNumber": booking.bookingNumber,
+              "Reservation name": booking.firstName,
+              "CheckInDate": booking.startDate,
+              "CheckOutDate": booking.endDate,
+              "NumberOfGuests": booking.visitors,
+              "NumberOfRooms": bookedRooms.length
+            }
+            bookedRoomResponse.push(bookingInformation);
+        });
         // Checks if there is any booked room and then returns a response
-        if (bookedRooms.length > 0) {
-            return sendResponse(200, { rooms: bookedRooms });
+        if (bookedRoomResponse.length > 0) {
+            return sendResponse(200, { rooms: bookedRoomResponse });
         } else {
             return sendResponse(404, { message: 'Inga rum hittades.' });
         }
 
-        // Catches any errors and returns a response message
     } catch (error) {
-        return sendResponse(500, { success: false, message: error });
+        // Log the error details
+        console.error('Error:', error);
+
+        // Return a 500 response with an error message
+        return sendResponse(500, { success: false, message: 'Internal Server Error' });
     }
 }
