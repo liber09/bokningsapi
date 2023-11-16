dd872747d2add15c0415e84a24fffa17b92c1494
liber09 <linda.bergsangel@hotmail.com>
2023-11-16 16:05:34 +0100

    Can get rooms

--- functions/getBookings/index.js
+++ functions/getBookings/index.js
@@ -1,34 +1,25 @@
-const { sendResponse } = require('../../responses/index.js');
 const AWS = require('aws-sdk');
+const { sendResponse } = require('../../responses/index');
+
 const db = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event, context) => {
-  const rooms = [];
-  const { Items } = await db
-    .scan({
-      TableName: 'booking-db',
-    })
-    .promise();
 
-  if (Items.Items && Items.length > 0) {
-    Items.Items.array.forEach((activeBooking) => {
-      let booking = {
-        id: activeBooking.id,
-        bookingNumber: activeBooking.bookingNumber,
-        startDate: activeBooking.startDate,
-        endDate: activeBooking.endDate,
-        visitors: activeBooking.visitors,
-        //roomCount: activeBooking.roomCount
-        name: activeBooking.firstName,
-      };
-      rooms.put(booking);
-    });
-  } else {
-    return sendResponse(204, { message: 'Inga rum hittades.' });
-  }
-  if (rooms.length > 0) {
-    return sendResponse(200, { success: true, event: rooms });
-  } else {
-    return sendResponse(204, { message: 'Inga rum hittades.' });
-  }
-};
+
+exports.handler = async (event, context) => {
+    try {
+        const params = {
+            TableName: 'room-db',
+        };
+
+        const result = await db.scan(params).promise();
+
+        if (result.Items && result.Items.length > 0) {
+            return sendResponse(200, { rooms: result.Items });
+        } else {
+            return sendResponse(404, { message: 'Inga rum hittades.' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return sendResponse(500, { success: false, message: 'Misslyckades med att hämta rum från DynamoDB.' });
+    }
+}
