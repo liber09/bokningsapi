58ec60ea7c9f7baae32ba8ed2609ad233790db75
Marlon <marlongarciab@hotmail.com>
2023-11-16 18:28:24 +0100

    Changes

--- functions/addBooking/index.js
+++ functions/addBooking/index.js
@@ -1,10 +1,8 @@
 const { sendResponse } = require('../../responses/index.js');
-const nanoid = require('nanoid');
 const uuid = require('uuid');
 const moment = require('moment');
 const AWS = require('aws-sdk');
 const db = new AWS.DynamoDB.DocumentClient();
-const ses = new AWS.SES();
 
 exports.handler = async (event, context) => {
   const newBooking = JSON.parse(event.body);
@@ -41,6 +39,10 @@ exports.handler = async (event, context) => {
     );
     console.log('booked rooms:', bookedRooms);
 
+    if (bookedRooms.length === 0) {
+        return sendResponse(400, { success: false, error: 'No available rooms' });
+      }
+
     // Beräkna totalbeloppet baserat på rumstyper och nätter
     const totalAmount = calculateTotalAmount(
       roomTypesToBook,
@@ -49,23 +51,7 @@ exports.handler = async (event, context) => {
     );
     console.log('total amount:', totalAmount);
 
-    // Generera ett bokningsnummer (implementera din egen logik)
-    //const bookingNumber = generateBookingNumber();
-
-    // Skicka bekräftelsemail
-    /*
-    const confirmationEmail = generateConfirmationEmail({
-      bookingNumber,
-      numberOfGuests: visitors.length,
-      roomTypes,
-      totalAmount,
-      checkInDate: startDate,
-      checkOutDate: endDate,
-      guestName: firstname,
-    });
-    */
-
-    // Spara bokningsdetaljer i DynamoDB
+  // Spara bokningsdetaljer i DynamoDB
 
     await db
       .put({
@@ -82,16 +68,6 @@ exports.handler = async (event, context) => {
       })
       .promise();
 
-    // Skicka bekräftelsemail
-    /*
-    await sendEmail(email, 'Bokningsbekräftelse', confirmationEmail);
-
-    return sendResponse(200, { success: true, bookingNumber, totalAmount });
-  } catch (error) {
-    console.error('Fel:', error);
-    return sendResponse(500, { success: false });
-  }
-  */
     return sendResponse(200, { success: true, newBooking });
   } catch (error) {
     console.log('error from exports.handler', error);
@@ -125,61 +101,6 @@ exports.handler = async (event, context) => {
 
     return totalCost;
   }
-
-  function generateConfirmationEmail({
-    bookingNumber,
-    numberOfGuests,
-    roomTypes,
-    totalAmount,
-    checkInDate,
-    checkOutDate,
-    guestName,
-  }) {
-    // Implementera logik för att generera innehållet i ett bekräftelsemail
-
-    // Exempel på innehåll i bekräftelsemailet (anpassa efter behov)
-    const emailContent = `
-        Thank you, ${guestName}, for your booking!
-
-        Bokningsnummer: ${bookingNumber}
-        Antal gäster: ${numberOfGuests}
-        Rumstyper: ${roomTypes.join(', ')}
-        Totalbelopp: ${totalAmount} SEK
-        Incheckningsdatum: ${checkInDate}
-        Utcheckningsdatum: ${checkOutDate}
-
-        We are looking forward to your visit!
-    `;
-
-    return emailContent;
-  }
-
-  async function sendEmail(toEmail, subject, body) {
-    const params = {
-      Destination: {
-        ToAddresses: [toEmail],
-      },
-      Message: {
-        Body: {
-          Text: {
-            Data: body,
-          },
-        },
-        Subject: {
-          Data: subject,
-        },
-      },
-      Source: 'din-email@example.com', // Uppdatera med din verifierade e-postadress i SES
-    };
-
-    try {
-      await ses.sendEmail(params).promise();
-    } catch (error) {
-      console.error('Fel vid sändning av e-post:', error);
-      throw error;
-    }
-  }
-};
 
 async function bookRooms(roomTypesToBook, availableRooms, newBooking) {
   const roomsToBook = [];
@@ -232,7 +153,8 @@ function validateParameters(firstName, eMail, startDate, endDate, visitors) {
   let validationError = false;
   let errorMessage = '';
   var validEmailRegex = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
-  // const today = moment(new Date()).format('YYYY-MM-DD');
+
+
   const today = moment(new Date(), 'YYYY-MM-DD').unix();
   if (firstName && firstName.trim() === '') {
     validationError = true;
@@ -334,3 +256,6 @@ function getDatesInRange(startDate, endDate) {
 
   return dates;
 }
+};
+
+
