98ee508bd78792efe32e8fd741a7282a542bfaa5
Niko0103 <125255783+Niko0103@users.noreply.github.com>
2023-11-17 14:03:17 +0100

    Delete function

--- functions/deleteBooking/index.js
+++ functions/deleteBooking/index.js
@@ -11,3 +11,49 @@ exports.handler = async (event, context) => {
 
   return sendResponse(200, { success: true, event: Items });
 };
+
+const bookings = [
+  { id: 1, name: 'Booking 1' },
+  { id: 2, name: 'Booking 2' },
+
+exports.handler = async (event, context) => {
+  // Check if the request is for deleting a booking
+  if (event.httpMethod === 'DELETE') {
+    // Extract the booking ID from the path parameters
+    const bookingId = event.pathParameters.id;
+
+    // Use the DynamoDB delete method to remove the booking
+    try {
+      await db
+        .delete({
+          TableName: 'booking-db',
+          Key: {
+            bookingId: bookingId,
+          },
+        })
+        .promise();
+
+      return sendResponse(200, { success: true, message: `Booking ${bookingId} deleted successfully` });
+    } catch (error) {
+      console.error('Error deleting booking:', error);
+      return sendResponse(500, { success: false, error: 'Internal Server Error' });
+    }
+  }
+
+  // If the request is not a DELETE request, perform a scan to retrieve all bookings
+  try {
+    const { Items } = await db
+      .scan({
+        TableName: 'booking-db',
+      })
+      .promise();
+
+    return sendResponse(200, { success: true, bookings: Items });
+  } catch (error) {
+    console.error('Error scanning bookings:', error);
+    return sendResponse(500, { success: false, error: 'Internal Server Error' });
+  }
+}
+
+
+]
