b5a3b577c28655636e6eece0872921f8a7ce13a0
Marlon <marlongarciab@hotmail.com>
2023-11-17 12:19:34 +0100

    commit delete function

--- functions/deleteBooking/index.js
+++ functions/deleteBooking/index.js
@@ -1,13 +1,20 @@
-const { sendResponse } = require('../../responses/index.js');
 const AWS = require('aws-sdk');
+const { sendResponse } = require('../../responses/index');
 const db = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event, context) => {
-  const { Items } = await db
-    .scan({
-      TableName: 'booking-db',
-    })
-    .promise();
+exports.deleteBooking = async (bookingNumber) => {
+  const params = {
+    TableName: 'booking-db',
+    Key: {
+      bookingNumber: bookingNumber,
+    },
+  };
 
-  return sendResponse(200, { success: true, event: Items });
+  try {
+    await db.delete(params).promise();
+    return true; // Return true if the booking is successfully deleted
+  } catch (error) {
+    console.error('Error:', error);
+    return false; // Return false if there is an error deleting the booking
+  }
 };
